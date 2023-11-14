e02eee4bc0c215c7058ce431a9967cccf5207872
Amarthya K R <amarthya10@gmail.com>
2023-11-14 18:51:06 +0530

    [FIX]: Raipur AQM API response format updated.

--- pkg/controllers/aqm_spatialforecast.go
+++ pkg/controllers/aqm_spatialforecast.go
@@ -3,15 +3,17 @@ package controllers
 import (
 	"iudx_domain_specific_apis/pkg/logger"
 	"iudx_domain_specific_apis/pkg/models"
+	"iudx_domain_specific_apis/pkg/responses"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 type AQMSpatialForecastRequestBody struct {
-	ForecastStart string `json:"forecastStart"`
-	ForecastEnd   string `json:"forecastEnd"`
-	GeoJson_Id    int32  `json:"geojson_id"`
+	ForecastStart time.Time `json:"forecastStart" time_format:"2006-01-02T15:04:05Z07:00"`
+	ForecastEnd   time.Time `json:"forecastEnd" time_format:"2006-01-02T15:04:05Z07:00"`
+	MeasuredValue string    `json:"measuredValue"`
 }
 
 type AQMSpatialForecasrController struct{}
@@ -25,7 +27,7 @@ func (ctrl AQMSpatialForecasrController) GetAQMSpatialForecast(c *gin.Context) {
 		return
 	}
 
-	aqmSpatialForecasts, err := aqmSpatialForecastModel.GetAQMSpatialForecasts(reqBody.ForecastStart, reqBody.ForecastEnd, reqBody.GeoJson_Id)
+	aqmSpatialForecasts, err := aqmSpatialForecastModel.GetAQMSpatialForecasts(reqBody.ForecastStart, reqBody.ForecastEnd, reqBody.MeasuredValue)
 
 	if err != nil {
 		logger.Error.Println(err.Error())
@@ -33,5 +35,5 @@ func (ctrl AQMSpatialForecasrController) GetAQMSpatialForecast(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, aqmSpatialForecasts)
+	c.JSON(http.StatusOK, responses.FormatAQMSpatialForecastResponse(aqmSpatialForecasts))
 }
--- pkg/responses/aqm_spatialforecast.go
+++ pkg/responses/aqm_spatialforecast.go
@@ -1,1 +1,62 @@
 package responses
+
+import (
+	"encoding/json"
+	"fmt"
+	"iudx_domain_specific_apis/pkg/models"
+)
+
+type TimeseriesAQMSpatialForecastResponse struct {
+	Timestamps []string            `json:"timestamps"`
+	GeoJson    [][]json.RawMessage `json:"geojson"`
+	Values     [][]float32         `json:"values"`
+}
+
+type PropertiesAQMSpatialForecastResponse struct {
+	MeasuredValue string `json:"measuredValue"`
+}
+
+type AQMSpatialForecastResponse struct {
+	Timeseries TimeseriesAQMSpatialForecastResponse `json:"timeseries"`
+	Properties PropertiesAQMSpatialForecastResponse `json:"properties"`
+}
+
+func FormatAQMSpatialForecastResponse(aqmSpatialForecasts []models.AQMSpatialForecast) AQMSpatialForecastResponse {
+	valueMap := make(map[string][]float32)
+	geoJsonMap := make(map[string][]json.RawMessage)
+
+	for _, aqmSpatialForecast := range aqmSpatialForecasts {
+		// ts := aqmSpatialForecast.ObservationDateTime.String()
+		ts := aqmSpatialForecast.ObservationDateTime.Format("2006-01-02T15:04:05+00:00")
+		valueMap[ts] = append(valueMap[ts], aqmSpatialForecast.PollutantVal)
+		geoJsonMap[ts] = append(geoJsonMap[ts], json.RawMessage(aqmSpatialForecast.GeoJson))
+	}
+
+	var timestamps []string
+	var values [][]float32
+	var geoJson [][]json.RawMessage
+
+	for timestamp, val := range valueMap {
+		timestamps = append(timestamps, timestamp)
+		values = append(values, val)
+	}
+
+	for _, geo := range geoJsonMap {
+		geoJson = append(geoJson, geo)
+	}
+
+	fmt.Println(timestamps)
+
+	resultAQMSpatialForecastResponse := AQMSpatialForecastResponse{
+		Timeseries: TimeseriesAQMSpatialForecastResponse{
+			Timestamps: timestamps,
+			GeoJson:    geoJson,
+			Values:     values,
+		},
+		Properties: PropertiesAQMSpatialForecastResponse{
+			MeasuredValue: "co2",
+		},
+	}
+
+	return resultAQMSpatialForecastResponse
+}
