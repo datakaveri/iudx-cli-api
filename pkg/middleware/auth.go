02cdcf6f868695abe5ec938e80edf3839c0dce8b
Amarthya K R <amarthya10@gmail.com>
2023-12-27 12:08:25 +0530

    [FEATURE]: JWT Auth Token added

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -1,10 +1,12 @@
 package middleware
 
 import (
+	"fmt"
 	"iudx_domain_specific_apis/pkg/configs"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func APIKeyAuthMiddleware() gin.HandlerFunc {
@@ -16,8 +18,20 @@ func APIKeyAuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		if configs.GetAPIKey() == apiKeyToken {
-			c.Next()
+		token, _, err := new(jwt.Parser).ParseUnverified(apiKeyToken, jwt.MapClaims{})
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Error with JWT Token"})
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok {
+			iid := fmt.Sprint(claims["iid"])
+			if configs.GetAPIKey() == iid {
+				c.Next()
+			} else {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Invalid JWT Token"})
+				return
+			}
 		} else {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
 			return
